4cd2d857825d7025cede802a02769982bcb735ac
SanjuMu <sanjusri.munigala@gmail.com>
2024-12-16 19:32:11 -0500

    floyd

---
+++ floyd.py
@@ -0,0 +1,9 @@
+print("Floyd triangle: ")
+n = int(input("enter the numbers of rows: "))
+x = 1
+
+for i in range(n):
+    for j in range(i + 1):
+        print(x,end= " ")
+        x= x+1
+    print()
